6b44835247a22851314c8d743bea6f1d1f90ed67
Gustavo Grosbelli <gustavogrosbelli11@gmail.com>
2026-03-31 20:41:31 -0300

    incio da implementação API
    Feito a chamada da API que irá buscar a imagem aleatório e demais dados

--- pages/RandomUser.js
+++ pages/RandomUser.js
@@ -1,8 +1,49 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+import { useEffect, useState } from "react";
+
+
 export function RandomUser() {
+
+  const [dados, setDados] = useState(null);
+
+
+  useEffect (async  () => {
+      try {
+          const response = await fetch("https://randomuser.me/api/");
+          const data = await response.json();
+          setDados(data.results[0]);
+      }
+      catch (error) { 
+          console.error("Erro ao buscar dados do usuário:", error);
+      }
+  }, []);
+
   return (
-    <View>
-      <Text>GERADOR DE PERFIL</Text>
+    <>
+       <Text style={styles.title}>GERADOR DE PERFIL</Text>
+    <View style={styles.container}>
+
+      <Image style={styles.image}
+        source={{ uri: dados?.picture?.large }}
+    />
     </View>
+  </>
   );
 }
+
+
+const styles = StyleSheet.create({
+  
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 150,
+  },
+  
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+});
