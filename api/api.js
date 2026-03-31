8d5656ff97c933648e1fe3d54d7746ffb5223f78
Gabriel Maran <gabriel.enzo.libero.maran@gmail.com>
2026-03-31 10:47:23 -0300

    feat: implementando conexão com a API do Gemini e iniciando chatbot

--- api/api.js
+++ api/api.js
@@ -1,36 +1,25 @@
-//npm install dotenv
-//npx expo install expo-constant
+import { GoogleGenAI } from "@google/genai";
+
 //npm install @google/generative-ai
+const ai = new GoogleGenAI({ apiKey: process.env.EXPO_PUBLIC_API_KEY });
 
-import { GoogleGenerativeAI } from "@google/generative-ai";
-// 1. Configure a chave aqui (ou use process.env.GEMINI_API_KEY)
-const genAI = new GoogleGenerativeAI(process.env.EXPO_PUBLIC_GEMINI_API_KEY);
-
-// 2. Pega perfil aleatorio (Corrigido para /api)
+// Pega Perfil Aleatorio de usuario
 export async function getRandomUser() {
   const response = await fetch("https://randomuser.me");
   const data = await response.json();
   return data.results[0];
 }
 
-async function enviarParaIA(textoUsuario) {
-  try {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-    const result = await model.generateContent(textoUsuario);
-    const response = await result.response;
-
-    return response.text();
-  } catch (erro) {
-    console.error("Erro na comunicação com a IA:", erro);
-    return "Erro ao processar mensagem.";
-  }
-}
-
-async function exemplo() {
-  const usuario = await getRandomUser();
-  const pergunta = `O nome do usuário é ${usuario.name.first}. Crie um apelido para ele.`;
-
-  const resposta = await enviarParaIA(pergunta);
-  console.log(resposta);
+export async function sendToGeminiIA(prompt) {
+  const response = await ai.models.generateContent({
+    model: "gemini-3-flash-preview",
+    systemInstruction:
+      "Responda de forma extremamente resumida e direta, usando tópicos se necessário. Seja transparente, caso não saiba de algo ou não tem certeza, deixe especificado",
+    contents: prompt,
+    config: {
+      temperature: 0.1,
+    },
+  });
+  console.log(response.text);
+  return response.text;
 }
